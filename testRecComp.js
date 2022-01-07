5f6d9a57b0bdbe76435aa81d15980d58a9760199
karia19 <39730014+karia19@users.noreply.github.com>
2022-01-07 12:04:44 +0200

    Update testRecComp.js

--- testRecComp.js
+++ testRecComp.js
@@ -184,6 +184,10 @@ const audioRecorder =  () => {
         <div>
         <Head>
             <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.12.1/css/all.css" crossOrigin="anonymous" />
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossOrigin="anonymous"></script>
+            <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossOrigin="anonymous"/>
+
 
         </Head>
         <div  className={styles.main}>
